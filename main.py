0e24951391a00011d72bd9f676e8274737c5ce5c
hangsinghui <shhang.2021@scis.smu.edu.sg>
2023-05-11 11:23:38 +0800

    updates

--- main.py
+++ main.py
@@ -1,14 +1,57 @@
-#Create currency class
+import json
+
+f = open("sample_events.json")
+
+obj = json.load(f)
+
+currency_states = {}
+
+trade_events = {}
+#Key: eventID, Value: Currency object
+
+date_dict={"1M": 30, "2M": 60, "3M": 90,"6M":180, "9M":270, "12M":360 }
 
 class Currency():
-    def __init__(self,m,b,rate,DivisorRatio, spread):
-        #Store values of 
-        
+    def __init__(self):
+        self.rate = None
+        self.ccy = None
+        self.m = None
+        self.b = None
+        self.DivisorRatio = None
+        self.spread = None
+        self.tenor = None
+        self.quantity = None
+
+for event in obj:
+    #Get event type
+    id  = event["EventId"]
+    id = Currency()
+    if event["EventType"] == "TradeEvent":
+        #Extract Tenor and position
+        id.tenor = date_dict[event["Tenor"]]
+        id.position = event["Quantity"]
+    elif event["EventType"] == "FXMidEvent":
+        #Extract rate
+        #Extract ccy
         pass
-        
-#To identify an event through ccy and tenor
+    else:
+        #Extract m, b ,divsior, spread
+        pass
+    
+    #Store in currency dictionary
+    currency_states[id] = id
 
-#Function to query for buy sell 
 
-#If not in currency list, create a new currency object
+
+
+
+
+
+
+
+changes = {}
+
+
+
+
 

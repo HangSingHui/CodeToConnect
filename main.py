6ed3d7c259579d2cdabdbb1e6093b12e58497b58
hangsinghui <shhang.2021@scis.smu.edu.sg>
2023-05-11 10:34:04 +0800

    pdate

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #Create currency class
 
 class Currency():
-    def __init__(self,)
+    def __init__(self,m,b,rate,DivisorRatio, spread):
+        #Store values of 
+        
+        pass
+        
+#To identify an event through ccy and tenor
+
+#Function to query for buy sell 
+
+#If not in currency list, create a new currency object
+
